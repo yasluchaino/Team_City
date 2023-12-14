5cb8e26eb4879dc18e7fb0da32b19bad3f7b38b3
ankey18 <anna2002f@yandex.ru>
2023-12-14 17:53:35 +0300

    add test

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -1,14 +1,46 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-
+using Factorial;
 namespace UnitTestProject1
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestFactorialOf7()
         {
+            Fact f = new Fact();
+            int result = f.Factorial(7);
+            Assert.AreEqual(5040, result);
+        }
+
+        [TestMethod]
+        public void TestFactorialOf8()
+        {
+            Fact f = new Fact();
+            int result = f.Factorial(8);
+            Assert.AreEqual(40320, result);
+        }
+        [TestMethod]
+        public void TestFactorialOf0()
+        {
+            Fact f = new Fact();
+            int result = f.Factorial(0);
+            Assert.AreEqual(1, result);
+        }
+        [TestMethod]
+        public void TestFactorialOf1()
+        {
+            Fact f = new Fact();
+            int result = f.Factorial(1);
+            Assert.AreEqual(1, result);
+        }
+        [TestMethod]
+        public void TestFactorialOf5()
+        {
+            Fact f = new Fact();
+            int result = f.Factorial(5);
+            Assert.AreEqual(120, result);
         }
     }
 }
