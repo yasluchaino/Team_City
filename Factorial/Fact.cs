4d09ab6a8d29ae93c007aa640b8d256c3071d321
ankey18 <anna2002f@yandex.ru>
2023-12-14 17:39:18 +0300

    add files

---
+++ Factorial/Fact.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    public class Fact
+    {
+        public Fact() { }
+
+        public int Factorial(int n)
+        {
+            int f = 1;
+            for (int i = 2; i <= n; i++)
+            {
+                f *= i;
+            }
+            return f;
+        }
+    }
+}
---
+++ Factorial/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Factorial
+{
+    public partial class Form1 : Form
+    {
+        Fact f;
+        public Form1()
+        {
+            InitializeComponent();
+            f = new Fact();
+            textBox2.Text = "0";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+   
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var res = f.Factorial(int.Parse(textBox2.Text));
+            label2.Text = res.ToString();
+        }
+    }
+}
